ac7c8b7037fcd44a7175b5bc94799b0c346d3d2c
Bartosz Rybarczyk <rybarczyk.bartosz@o2.pl>
2026-05-28 12:56:57 +0200

    smfl

--- pvz/main.cpp
+++ pvz/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "chuj" << endl;
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Test SFML 2.6.2 dzia³a!");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    shape.setPosition(100.f, 100.f);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.draw(shape);
+
+        window.display();
+    }
+
     return 0;
 }
