0ab7989a7777edfb427946b7d1da3dc37e37adc1
Bartosz Rybarczyk <rybarczyk.bartosz@o2.pl>
2026-05-29 13:21:19 +0200

    sfml to syf

--- pvz/main.cpp
+++ pvz/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     std::cout << "dziallaaa" << std::endl;
     std::cout << "dziallaaaaaaaaaaaaaaa" << std::endl;
+    std::cout << "jebac sfml" << std::endl;
     sf::RenderWindow window(sf::VideoMode(400, 400), "Test SFML 2.6.2 dzia³a!");
 
     sf::CircleShape shape(100.f);
